7beab9c48da133e8921a6e9837d1e1e6c9647fba
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-05 16:51:26 -0700

    Update Script Collection.ps1

--- Script Collection.ps1
+++ Script Collection.ps1
@@ -1,23 +1,46 @@
-# Module 1: Assign static IP and DNS
-# Prompt user for IP address, subnet mask, default gateway, and DNS server
-$IP = Read-Host "Enter IP Address"
-$SubnetMask = Read-Host "Enter Subnet Mask"
-$Gateway = Read-Host "Enter Default Gateway"
-$DNS = Read-Host "Enter DNS Server"
-# Set the static IP address and DNS server
-New-NetIPAddress -InterfaceAlias Ethernet -IPAddress $IP -PrefixLength $SubnetMask -DefaultGateway $Gateway
-Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses $DNS
-# Module 2: Rename the Windows Server VM
-# Prompt user for a new name for the Windows Server VM and rename it
-$NewName = Read-Host "Enter a new name for the Windows Server VM"
-Rename-Computer -NewName $NewName -Restart
-# Module 3: Install AD-Domain-Services
-# Install the AD-Domain-Services feature
-Install-WindowsFeature -Name AD-Domain-Services
-# Module 4: Create AD Forest
+# Module 1: Assign static IP and DNS 
+# Prompt user for IP address, subnet mask, default gateway, and DNS server 
+$IP = Read-Host "Enter IP Address" 
+$SubnetMask = Read-Host "Enter Subnet Mask" 
+$Gateway = Read-Host "Enter Default Gateway" 
+$DNS = Read-Host "Enter DNS Server" 
+# Set the static IP address and DNS server 
+New-NetIPAddress -InterfaceAlias Ethernet -IPAddress $IP -PrefixLength $SubnetMask -DefaultGateway $Gateway 
+Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses $DNS 
+# Module 2: Rename the Windows Server VM 
+# Prompt user for a new name for the Windows Server VM and rename it 
+$NewName = Read-Host "Enter a new name for the Windows Server VM" 
+Rename-Computer -NewName $NewName -Restart 
+# Module 3: Install AD-Domain-Services 
+# Install the AD-Domain-Services feature 
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
+Install-WindowsFeature RSAT-AD-Powershell
+# Module 4: Create AD Forest, OU, and Users 
+# Prompt user for Forest name, OU name and user details 
 
+$OUName = Read-Host "Enter OU Name" 
+$AdminUser = Read-Host "Enter Administrator Username" 
+$AdminPassword = Read-Host -AsSecureString "Enter Administrator Password" 
+$UserName = Read-Host "Enter User Name" 
+$UserPassword = Read-Host -AsSecureString "Enter User Password" 
+# Create AD Forest 
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "corp.greendata.com" `
+-DomainNetbiosName "CORP" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
 # Create new Organizational Units (OU) in AD
-New-ADOrganizationalUnit -Name “Engineering” -Path “DC=corp,DC=corp.greendata.com”
+New-ADOrganizationalUnit -Name “Engineering” -Path “DC=corp,DC=greendata,DC=com”
 # Create new users in AD
-$securePassword = ConvertTo-SecureString “password” -AsPlainText -Force
-New-ADUser -Name “Jordan Marshall” -AccountPassword $securePassword -Path “OU=Engineering,DC=example,DC=com” -Enabled $true
+# Create new Organizational Units (OU) in AD 
+New-ADOrganizationalUnit -Name “Engineering” -Path “DC=corp,DC=corp.greendata.com” 
+# Create new users in AD 
+New-Aduser -Name "Jordan Marshall" -GivenName "Jordan" -Surname "Marshall" -SamAccountName "JMarshall" -Title "TPS Reporting Lead" -company "GreenData" -Department "TPS" -EmailAddress "MarshallJordan@greendata.com" -Path “OU=Engineering,DC=corp,DC=greendata,DC=com”
