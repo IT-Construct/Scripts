4ef4539dbb1aff6f9e46187d5d8d0955f7a14eb8
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 15:45:30 -0700

    Update and rename Module 3.ps1 to Module 2.ps1

--- Module 2.ps1
+++ Module 2.ps1
@@ -1,9 +1,8 @@
-
-# Module 3: Install AD-Domain-Services 
+# Module 2: Install AD-Domain-Services 
 # Install the AD-Domain-Services feature 
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
 Install-WindowsFeature RSAT-AD-Powershell
-# Module 4: Create AD Forest, OU, and Users 
+
 # Prompt user for Forest name, OU name and user details 
 # Create AD Forest 
 Import-Module ADDSDeployment
