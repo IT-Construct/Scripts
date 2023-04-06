6678008336190d43c68234317a0dc7246c8b61c5
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-05 19:08:20 -0700

    Create Module 3.ps1

---
+++ Module 3.ps1
@@ -0,0 +1,21 @@
+
+# Module 3: Install AD-Domain-Services 
+# Install the AD-Domain-Services feature 
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
+Install-WindowsFeature RSAT-AD-Powershell
+# Module 4: Create AD Forest, OU, and Users 
+# Prompt user for Forest name, OU name and user details 
+# Create AD Forest 
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "corp.greendata.com" `
+-DomainNetbiosName "CORP" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
