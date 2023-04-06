973c2104e4056adef26b40cab48fc057bd363282
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 15:43:57 -0700

    Update and rename Module 1 & 2.ps1 to Module 1.ps1

--- Module 1.ps1
+++ Module 1.ps1
@@ -1,13 +1,13 @@
 # Module 1: Assign static IP and DNS 
 # Prompt user for IP address, subnet mask, default gateway, and DNS server 
+$NewName = Read-Host "Enter a new name for the Windows Server VM" 
+Rename-Computer -NewName $NewName
+
 $IP = Read-Host "Enter IP Address" 
-$SubnetMask = Read-Host "Enter Subnet Mask" 
+$SubnetMask = Read-Host "Enter CIDR # " 
 $Gateway = Read-Host "Enter Default Gateway" 
 $DNS = Read-Host "Enter DNS Server" 
+
 # Set the static IP address and DNS server 
 New-NetIPAddress -InterfaceAlias Ethernet -IPAddress $IP -PrefixLength $SubnetMask -DefaultGateway $Gateway 
 Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses $DNS 
-# Module 2: Rename the Windows Server VM 
-# Prompt user for a new name for the Windows Server VM and rename it 
-$NewName = Read-Host "Enter a new name for the Windows Server VM" 
-Rename-Computer -NewName $NewName -Restart 
