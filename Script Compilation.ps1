0d1bc5e6a4cdb74694929dedee5ebda9ef0ae0ae
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 15:41:24 -0700

    Update Script Compilation.ps1

--- Script Compilation.ps1
+++ Script Compilation.ps1
@@ -4,17 +4,23 @@ $IP = Read-Host "Enter IP Address"
 $SubnetMask = Read-Host "Enter CIDR # " 
 $Gateway = Read-Host "Enter Default Gateway" 
 $DNS = Read-Host "Enter DNS Server" 
+
 # Set the static IP address and DNS server 
 New-NetIPAddress -InterfaceAlias Ethernet -IPAddress $IP -PrefixLength $SubnetMask -DefaultGateway $Gateway 
 Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses $DNS 
+
 # Module 2: Rename the Windows Server VM 
 # Prompt user for a new name for the Windows Server VM and rename it 
 $NewName = Read-Host "Enter a new name for the Windows Server VM" 
 Rename-Computer -NewName $NewName -Restart 
+
+
 # Module 3: Install AD-Domain-Services 
 # Install the AD-Domain-Services feature 
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
 Install-WindowsFeature RSAT-AD-Powershell
+
+
 # Module 4: Create AD Forest, OU, and Users 
 # Prompt user for Forest name, OU name and user details 
 # Create AD Forest 
@@ -31,7 +37,39 @@ Install-ADDSForest `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
-# Create new Organizational Units (OU) in AD
-New-ADOrganizationalUnit -Name “Engineering” -Path “DC=corp,DC=greendata,DC=com”
-# Create new users in AD 
-New-Aduser -Name "Jordan Marshall" -GivenName "Jordan" -Surname "Marshall" -SamAccountName "JMarshall" -Title "TPS Reporting Lead" -company "GreenData" -Department "TPS" -EmailAddress "MarshallJordan@greendata.com" -Path “OU=Engineering,DC=corp,DC=greendata,DC=com”
+
+# Import the ActiveDirectory module
+Import-Module ActiveDirectory
+
+# Set the path to the CSV file
+$csvPath = "C:\Users\Administrator\Downloads\Green Data - Sheet1.csv"
+
+# Read the CSV file and loop through each row
+Import-Csv $csvPath | ForEach-Object {
+
+    # Store the user information in variables
+    $name = $_.Name
+    $firstName = $_.GivenName
+    $lastName = $_.Surname
+    $title = $_.Title
+    $username = $_.samAccountName
+    $department = $_.Department
+
+    # Check if the organizational unit exists, create it if it doesn't
+    $ou = Get-ADOrganizationalUnit -Filter { Name -eq $department } -ErrorAction SilentlyContinue
+    if (!$ou) {
+        New-ADOrganizationalUnit -Name $department -Path "DC=corp,DC=greendata,DC=com"
+    }
+
+    # Check if the user already exists, create it if it doesn't
+    $user = Get-ADUser -Filter { SamAccountName -eq $username } -ErrorAction SilentlyContinue
+    if (!$user) {
+        New-ADUser `
+            -Name $name `
+            -GivenName $firstName `
+            -Surname $lastName `
+            -Title $title `
+            -SamAccountName $username `
+            -Path “OU=$department,DC=corp,DC=greendata,DC=com”
+    }
+}
