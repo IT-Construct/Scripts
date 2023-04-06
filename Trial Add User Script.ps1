57ce6bf727eed426fdb6f325976acd18795165bc
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 15:46:56 -0700

    Delete Trial Add User Script.ps1

--- Trial Add User Script.ps1
+++
@@ -1,4 +0,0 @@
-$users = Import-Csv -Path "C:\path\to\file.csv"
-foreach ($user in $users) {
-    New-ADUser -Name $user.Name -GivenName $user.GivenName -Surname $user.Surname -Title $user.Title -Department $user.Department -SamAccountName $user.samAccountName -Path "OU=$user.Department DC=corp,DC=greendata,DC=com"
-}
