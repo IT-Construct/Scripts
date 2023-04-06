0b2d79a91982092d7c08a22c139945cfc0559751
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 10:22:19 -0700

    Update Module 4 Trial.ps1

--- Module 4 Trial.ps1
+++ Module 4 Trial.ps1
@@ -1,11 +1,11 @@
 echo "Please input employee's information"
-$name Read-Host -Prompt "Full Name: "
-$gname = Read-Host -Prompt "Given Name:
+$name = Read-Host -Prompt "Full Name: "
+$gname = Read-Host -Prompt "Given Name: "
 $sname = Read-Host -Prompt "Surname: "
-$acc = Read-Host -Prompt "Account Name:
-Stitle=Read-Host -Prompt "Title: "
+$acc = Read-Host -Prompt "Account Name: "
+Stitle = Read-Host -Prompt "Title: "
 $com = Read-Host -Prompt "Company: "
-$dep Read-Host -Prompt "Department: "
+$dep = Read-Host -Prompt "Department: "
 $email = Read-Host -Prompt "Email Address: "
 $org = Read-Host -Prompt "Organization: "
 # Create new Organizational Units (OU) in AD
