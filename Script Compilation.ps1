8fd8bf4c64047002a252cfef3d21a485e94a47fe
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 15:43:11 -0700

    Update Script Compilation.ps1

--- Script Compilation.ps1
+++ Script Compilation.ps1
@@ -1,5 +1,8 @@
 # Module 1: Assign static IP and DNS 
 # Prompt user for IP address, subnet mask, default gateway, and DNS server 
+$NewName = Read-Host "Enter a new name for the Windows Server VM" 
+Rename-Computer -NewName $NewName
+
 $IP = Read-Host "Enter IP Address" 
 $SubnetMask = Read-Host "Enter CIDR # " 
 $Gateway = Read-Host "Enter Default Gateway" 
@@ -9,19 +12,14 @@ $DNS = Read-Host "Enter DNS Server"
 New-NetIPAddress -InterfaceAlias Ethernet -IPAddress $IP -PrefixLength $SubnetMask -DefaultGateway $Gateway 
 Set-DnsClientServerAddress -InterfaceAlias Ethernet -ServerAddresses $DNS 
 
-# Module 2: Rename the Windows Server VM 
-# Prompt user for a new name for the Windows Server VM and rename it 
-$NewName = Read-Host "Enter a new name for the Windows Server VM" 
-Rename-Computer -NewName $NewName -Restart 
 
-
-# Module 3: Install AD-Domain-Services 
+# Module 2: Install AD-Domain-Services 
 # Install the AD-Domain-Services feature 
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Restart
 Install-WindowsFeature RSAT-AD-Powershell
 
 
-# Module 4: Create AD Forest, OU, and Users 
+# Module 3: Create AD Forest, OU, and Users 
 # Prompt user for Forest name, OU name and user details 
 # Create AD Forest 
 Import-Module ADDSDeployment
