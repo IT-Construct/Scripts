d960d503669c1d2bc8d542cb8dfb77edc9ae688e
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 15:45:59 -0700

    Update and rename Module 4.ps1 to Module 3.ps1

--- Module 4.ps1
+++
@@ -1,4 +0,0 @@
-# Create new Organizational Units (OU) in AD
-New-ADOrganizationalUnit -Name “Engineering” -Path “DC=corp,DC=greendata,DC=com”
-# Create new users in AD 
-New-Aduser -Name "Jordan Marshall" -GivenName "Jordan" -Surname "Marshall" -SamAccountName "JMarshall" -Title "TPS Reporting Lead" -company "GreenData" -Department "TPS" -EmailAddress "MarshallJordan@greendata.com" -Path “OU=Engineering,DC=corp,DC=greendata,DC=com”
