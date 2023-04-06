767e821e6d6c4703fda13c651071b8b1b3e97269
John Alfred C Alona <120781845+redalona@users.noreply.github.com>
2023-04-06 15:11:45 -0700

    Update Script Compilation.ps1

--- Script Compilation.ps1
+++ Script Compilation.ps1
@@ -1,7 +1,7 @@
 # Module 1: Assign static IP and DNS 
 # Prompt user for IP address, subnet mask, default gateway, and DNS server 
 $IP = Read-Host "Enter IP Address" 
-$SubnetMask = Read-Host "Enter Subnet Mask" 
+$SubnetMask = Read-Host "Enter CIDR # " 
 $Gateway = Read-Host "Enter Default Gateway" 
 $DNS = Read-Host "Enter DNS Server" 
 # Set the static IP address and DNS server 
